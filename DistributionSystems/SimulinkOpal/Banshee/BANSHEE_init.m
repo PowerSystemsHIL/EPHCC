54fee02ac4b93b32d1cdf6081ff3a4f8e3ca1555
Reynaldo Salcedo <rsalcedo@ll.mit.edu>
2016-09-22 14:47:09 -0400

    changed init file - cb_mech_delay - to reflect the breaker update

--- DistributionSystems/SimulinkOpal/Banshee/BANSHEE_init.m
+++ DistributionSystems/SimulinkOpal/Banshee/BANSHEE_init.m
@@ -1,20 +1,20 @@
 % Copyright (c) 2016 Massachusetts Institute of Technology
-% Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+% Permission is hereby granted, free of charge, to any person obtaining a copy of this
 % software and associated documentation files (the "Software"), to deal in the Software
-% without restriction, including without limitation the rights to use, copy, modify, 
-% merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
-% permit persons to whom the Software is furnished to do so, subject to the following 
+% without restriction, including without limitation the rights to use, copy, modify,
+% merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+% permit persons to whom the Software is furnished to do so, subject to the following
 % conditions:
-% 
-% 
-% The above copyright notice and this permission notice shall be included in 
+%
+%
+% The above copyright notice and this permission notice shall be included in
 % all copies or substantial portions of the Software.
-% 
-% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-% INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-% PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
-% OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+% INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+% PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+% OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 % SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 %
 %
@@ -35,10 +35,13 @@ relay_settings_init;
 cable_impedance_init;
 
 %% adds the component folders to the path
-% eventually, Opal-RT may need to include flexibility to use subfolders in a development 
+% eventually, Opal-RT may need to include flexibility to use subfolders in a development
 % components_path_init;
 
+
 %% circuit breaker delays
-% breaker mechanical delay in seconds
-%  - format: [open_delay, close_delay]
-cb_mech_delay = [5, 5]./60;
+% breaker mechanical open/close delay in seconds
+% different delays may be applied to each breaker, however,
+% for convenience it is assumed all breaker have the same delay 
+cb_mech_delay = 5/60;
+
