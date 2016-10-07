76eb8ad3cba8d94df946440f56ccb25ac74d89ce
Reynaldo Salcedo <rsalcedo@ll.mit.edu>
2016-10-07 17:31:43 -0400

    removed test procedure blocks

--- DistributionSystems/SimulinkOpal/Device_Integration_Test_01/INTEGRATION_TEST_01_init.m
+++ DistributionSystems/SimulinkOpal/Device_Integration_Test_01/INTEGRATION_TEST_01_init.m
@@ -26,14 +26,7 @@ components_path_init;
 clear;
 %% model configuration parameters
 Ts = 100E-6; % integration time
-<<<<<<< HEAD
 SIM_HIL = 1; % control mode for NG_CHP [1: Woodward, 0: manual/simulation]
-% TestProcedure_xlsRead;
-=======
-SIM_HIL = 0; % control mode for NG_CHP [1: Woodward, 0: manual/simulation]
-TestProcedure_xlsRead;
->>>>>>> master
-
 
 %% read relay settings
 % reads-in all relay settings for the system and initializes vectors to
