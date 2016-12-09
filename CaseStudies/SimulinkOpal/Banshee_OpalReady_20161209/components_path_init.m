898657f97845ace8816b357fb23c1884969573f2
Reynaldo Salcedo <rsalcedo@ll.mit.edu>
2016-12-08 21:56:27 -0500

    created banshee opal-ready case for testing - implemented lessons learned from banshee_20161108

---
+++ CaseStudies/SimulinkOpal/Banshee_OpalReady_20161209/components_path_init.m
@@ -0,0 +1,15 @@
+addpath('ActiveLoad');
+addpath('CHPandThermal');
+addpath('Cable');
+addpath('CircuitBreaker');
+addpath('Genset');
+addpath('HWController_Diesel_Genset_4MVA');
+addpath('HWController_NGCHP_Genset_3p5MVA');
+addpath('MeasurementBlocks');
+addpath('Motor');
+addpath('PassiveLoad');
+addpath('Relay');
+addpath('SEL751_InterfaceBlocks');
+addpath('Transformer');
+addpath('UDP');
+addpath('WoodwardEasyGen3500_InterfaceBlocks');
