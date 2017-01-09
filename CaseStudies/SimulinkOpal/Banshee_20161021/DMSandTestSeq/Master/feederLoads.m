514402845ae6b68868fecdfbbea2bfa9ea1cc051
ch24070 <chris.smith@ll.mit.edu>
2017-01-09 17:21:13 -0500

    Changes after testing the Test seq and DMS.

--- CaseStudies/SimulinkOpal/Banshee_20161021/DMSandTestSeq/Master/feederLoads.m
+++ CaseStudies/SimulinkOpal/Banshee_20161021/DMSandTestSeq/Master/feederLoads.m
@@ -5,7 +5,7 @@ f1(3) = loadLoad('..\..\I1.mat', tStop);
 f1(4) = loadLoad('..\..\C2.mat', tStop);
 f1(5) = loadLoad('..\..\I2.mat', tStop);
 f1_tot = sumFeeder(f1);
-plotLoad(f1, f1_tot, 'Feeder 1');
+%plotLoad(f1, f1_tot, 'Feeder 1');
 
 
 %% Feeder 2
@@ -18,7 +18,7 @@ f2(6) = loadLoad('..\..\I5.mat', tStop);
 f2(7) = loadLoad('..\..\C4.mat', tStop);
 f2(8) = loadLoad('..\..\P5.mat', tStop);
 f2_tot = sumFeeder(f2);
-plotLoad(f2, f2_tot, 'Feeder 2');
+%plotLoad(f2, f2_tot, 'Feeder 2');
 
 %% Feeder 3
 f3(1) = loadLoad('..\..\C5.mat', tStop);
@@ -27,4 +27,4 @@ f3(3) = loadLoad('..\..\P4.mat', tStop);
 f3(4) = loadLoad('..\..\C6.mat', tStop);
 f3(5) = loadLoad('..\..\P6.mat', tStop);
 f3_tot = sumFeeder(f3);
-plotLoad(f3, f3_tot, 'Feeder 3');
+%plotLoad(f3, f3_tot, 'Feeder 3');
