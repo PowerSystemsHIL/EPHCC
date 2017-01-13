a678e589f3fbeb1309e206f975419793ca61e19f
Reynaldo Salcedo <rsalcedo@ll.mit.edu>
2017-01-13 12:22:45 -0500

    removed old cases from casestudy dir

--- CaseStudies/SimulinkOpal/Banshee_20161021/mirror.m
+++
@@ -1,9 +0,0 @@
-function dataOut = mirror(x)
-% outputs a set of data that is x concatenated with a mirrored version of
-% itself so that x(end) = dataOut(length(x)+1) and the length is doubled
-[m,n] = size(x);
-if m >= n 
-    dataOut = [x; x(m:-1:1,:)];  % make more rows
-else
-    dataOut = [x, x(:,n:-1:1)];  % make more cols
-end
