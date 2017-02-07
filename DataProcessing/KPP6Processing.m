1254ef04a5877cbfad00eb35b47db273992ae9d7
Ed Corbett <ecorbett@ll.mit.edu>
2017-02-07 15:36:25 -0500

    Added PV and Battery data items

--- DataProcessing/KPP6Processing.m
+++ DataProcessing/KPP6Processing.m
@@ -2,14 +2,16 @@
 
 % This script fnds the amount of PV production (Actual KWh produced      %
 % divided by maximum possible KWh based on irradiance.                   %
-
 %
+%% Load data files
+load misc_3.dat;            % column 8 is irradiance
+load solar_kW.dat;          % column 2 is PV power, KW
 %% Demand
-%Irradiance=misc?(:,?);             %Irradiance in KW (1KW/m^2 times area)
-%AvailablePVKW=0.15*Irradiance;     %Assume 15% PV efficiency
+Irradiance=misc_3(:,8);             %Irradiance in KW (1KW/m^2 times area)
+AvailablePVKW=0.15*Irradiance;     %Assume 15% PV efficiency
 %
 %% TotalMilliseconds, TotalHours, deltaTSecs- Time parameters
-%time=misc?(:,1);
+time=misc_3(:,1);
 TotalMilliseconds=time(length(time))-time(1);
 TotalHours=TotalMilliseconds/1000/3600;
 deltaTSecs=(time(11)-time(10))/1000;
@@ -18,9 +20,9 @@ deltaTSecs=(time(11)-time(10))/1000;
 PVMaxKWh=sum(Irradiance)*deltaTSecs/3600;
 %
 %% Total PV KWh Produced
-%PVProducedKWh=sum(misc?(:,?))*deltaTSecs/3600;
+PVProducedKWh=sum(solar_kW(:,2))*deltaTSecs/3600;
 %
 %% Amount of PV Production
-PercentPVProduction=PvProducedKWh/PVMaxKWh;
+PercentPVProduction=PVProducedKWh/PVMaxKWh;
 disp ('Amount of PV Production, Percent');
 disp(PercentPVProduction);
--- DataProcessing/KPP7Processing.m
+++ DataProcessing/KPP7Processing.m
@@ -4,6 +4,7 @@
 %
 %% Diesel Emissions
 load powerreal.dat;
+load battery_kW.dat;
 time=powerreal(:,1);
 DieselKW=abs(powerreal(:,48));
 DieselCO2lbmcum=sum(DieselKW)/1000*1425*(time(11)-time(10))/1000/3600;
@@ -22,7 +23,7 @@ CHPCO2lbmcum=sum(CHPCO2lbmperhour)*(time(11)-time(10))/1000/3600;
 clear ngch*;
 %% Battery Cycles
 BattNamePlate=1000;                             %1000 KWh
-%BattCycles=sum(abs(misc?(:,?)))*deltaTSecs/3600/BattNamePlate/2;
+BattCycles=sum(abs(battery_kW(:,2)))*deltaTSecs/3600/BattNamePlate/2;
 %% Display Results
 disp ('Total Emissions in lbs CO2 for DERs is...')
 disp(CHPCO2lbmcum+DieselCO2lbmcum);
