5fb560a5273bd26712be03af9fe4ed9e8f4c7da3
Ed Corbett <ecorbett@ll.mit.edu>
2017-02-07 16:33:32 -0500

    Merged KPP1 Preprocessing and KPP1 Processing

--- DataProcessing/KPP1Processing.m
+++ DataProcessing/KPP1Processing.m
@@ -5,6 +5,34 @@
 % summaries                                                              %
 %
 %
+%% Load variables and rename
+load misc_1.dat%
+%
+time=misc_1(:,1);
+%
+I1=misc_1(:,2);
+I2=misc_1(:,3);
+I3=misc_1(:,4);
+I4=misc_1(:,5);
+I5=misc_1(:,6);
+I6=misc_1(:,7);
+%
+P1=misc_1(:,8);
+P2=misc_1(:,9);
+P3=misc_1(:,10);
+P4=misc_1(:,11);
+P5=misc_1(:,12);
+P6=misc_1(:,13);
+%
+C1=misc_1(:,14);
+C2=misc_1(:,15);
+C3=misc_1(:,16);
+C4=misc_1(:,17);
+C5=misc_1(:,18);
+C6=misc_1(:,19);
+%
+clear misc*;
+
 %% Demands
 I1Demand=300;
 I2Demand=250;
