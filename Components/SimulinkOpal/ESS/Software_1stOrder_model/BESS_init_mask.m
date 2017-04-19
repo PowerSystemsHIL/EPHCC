45c2676d4704e55b4406b9f6f4c34d98ea1de001
Opal-NREL3 <bryan.palmintier@nrel.gov>
2017-04-19 11:15:54 -0600

    BESS inverter updates to accomodate for variable power (the same bess model used for 3MW and 250kW units) Instantaited BESS and PV in Feeder 2 Added PV and ESS cb data to UDP strem now CB count is 49 (prev 47)

---
+++ Components/SimulinkOpal/ESS/Software_1stOrder_model/BESS_init_mask.m
@@ -0,0 +1,28 @@
+%% Required input parameters
+%CFG.Sn = 250e3;   %[VA]
+%CFG.fn = 60;    %[Hz]
+%CFG.Vn = 480;        %[V]
+%CFG.r_I_max; - ratio of Imax/In
+function [BP] = BESS_init_mask(CFG)
+
+%disp([ 'Init ESS: ' num2str(CFG.Sn/1000)]);
+BP.In = CFG.Sn/(CFG.Vn/sqrt(3))/3;
+BP.Imax = BP.In*CFG.r_I_max;
+
+
+%% Current source inverter - voltage source based
+% Inverter filter parameters
+BP.R_base = CFG.Vn^2/CFG.Sn;        %[ohm]
+BP.L_base = BP.R_base/2/pi/CFG.fn;     
+
+BP.R_PU = 5.4e-3;     %[PU]
+BP.L_PU = 10.2e-2;     %[PU]
+
+%series indctor resistatnce and impedance
+BP.R = BP.R_PU*BP.R_base;     %[ohm];
+BP.L = BP.L_PU*BP.L_base;     %[H];
+
+%disp([ 'R: ' num2str(BP.R) '/' num2str(BP.R_base)]);
+%disp([ 'L: ' num2str(BP.L) '/' num2str(BP.L_base)]);
+
+BP.I_slewrate = 1;
