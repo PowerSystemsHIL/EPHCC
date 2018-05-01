81053f8e09fd6fb61595638cafd3b91e593d7b4a
PKoralew <Przemyslaw.Koralewicz@nrel.gov>
2018-05-01 15:25:54 -0600

    Preparation for merge with MIT master branch: - Renamed distribution model to Ghost - Renamed library components modified by NREL by adding _nrel suffix

--- SimulationTools/esif-filexfer-py/upload_mgc3.bat
+++
@@ -1,3 +0,0 @@
-ren C:\MGC3\Workspace\Team3\6_TestSequence\TestSequence Team3
-python esif-upload.py -u pkoralew --project "9f9423f4-f085-4236-a2be-7895272d08a0" --subdirs --path "C:\MGC3\Workspace\Team3\6_TestSequence" --ext "mat" --tags "mgc2017" --dataset-tags "mgc2017"
-ren C:\MGC3\Workspace\Team3\6_TestSequence\Team3 TestSequence
