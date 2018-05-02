5149ae6a21e66bac7713eb49bb14bcac25ffec9d
pkoralew_Opal7 <przemyslaw.koralewicz@nrel.gov>
2018-05-02 12:50:29 -0600

    Added license disclaimer to TestSequence tools scripts

--- SimulationTools/TestSequence/plot_der.m
+++ SimulationTools/TestSequence/plot_der.m
@@ -1,3 +1,23 @@
+%% Copyright 2018 Alliance for Sustainable Energy, LLC
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+% and associated documentation files (the "Software"), to deal in the Software without restriction, 
+% including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+% and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+% subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+% BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS, THE COPYRIGHT HOLDERS, THE UNITED STATES, 
+% THE UNITED STATES DEPARTMENT OF ENERGY, OR ANY OF THEIR EMPLOYEES BE LIABLE FOR ANY CLAIM, 
+% DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%
+% Author: Przemyslaw Koralewicz / NREL
+% Date: 2017
+
 
 DERs = {struct('name', 'gen1', 'mcb_id', id.CBGen1, 'bus_id', id.CB112, 'Vn', 13800, 'prec', 1) ...
     struct('name', 'ess1', 'mcb_id', id.CBEss1, 'bus_id', id.CB207, 'Vn', 4800, 'prec', 1) ...
